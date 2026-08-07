9222adf8c0094fb045d0e15a0a39ab5af591c3a8
kwazedilla <kwazedilla@gmail.com>
2026-08-06 22:56:27 -0700

    add deep space skyboxes

--- assets/minecraft/shaders/include/skybox_config.glsl
+++ assets/minecraft/shaders/include/skybox_config.glsl
@@ -21,3 +21,39 @@
         fragColor = applyFog(fragColor,0.0);
         return;
     }
+
+    MODEL_SHADER(3) {
+        vec2 textureSize = vec2(1920,1080);
+
+        vec2 sphericalUV = normalToSpherical(worldDirection);
+        vec2 atlasUV = localTextureUV(textureSize,sphericalUV);
+        fragColor = texture(Sampler0,atlasUV);
+
+        fragColor.a = 1;
+        fragColor = applyFog(fragColor,0.0);
+        return;
+    }
+
+    MODEL_SHADER(4) {
+        vec2 textureSize = vec2(1920,1080);
+
+        vec2 sphericalUV = normalToSpherical(worldDirection);
+        vec2 atlasUV = localTextureUV(textureSize,sphericalUV);
+        fragColor = texture(Sampler0,atlasUV);
+
+        fragColor.a = 1;
+        fragColor = applyFog(fragColor,0.0);
+        return;
+    }
+
+    MODEL_SHADER(5) {
+        vec2 textureSize = vec2(1920,1080);
+
+        vec2 sphericalUV = normalToSpherical(worldDirection);
+        vec2 atlasUV = localTextureUV(textureSize,sphericalUV);
+        fragColor = texture(Sampler0,atlasUV);
+
+        fragColor.a = 1;
+        fragColor = applyFog(fragColor,0.0);
+        return;
+    }
